380016235f4891add38e0905c84d96435b94ef52
Regan Sarwas <regan_sarwas@nps.gov>
2014-12-30 15:59:20 -0900

    Add new view and SP to database document

--- tables.sql
+++ tables.sql
@@ -76,4 +76,63 @@ ALTER TABLE [dbo].[POI_PT_ChangeRequestAction_For_NPPlaces] CHECK CONSTRAINT [FK
 GO
 
 
+-- Insert Change Request Stored Procedure (to return auto id)
+-- Grant execute permission to public
+
+USE [akr_socio]
+GO
+
+/****** Object:  StoredProcedure [dbo].[POI_PT_ChangeRequest_For_NPPlaces_Insert]    Script Date: 12/30/2014 3:56:04 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author:		Regan Sarwas
+-- Create date: December 30, 2014
+-- Description:	Adds a new request to the database.
+-- =============================================
+CREATE PROCEDURE [dbo].[POI_PT_ChangeRequest_For_NPPlaces_Insert] 
+    @Operation  NVARCHAR(10), 
+    @Requestor NVARCHAR(50),
+    @Feature NVARCHAR(max)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @MyTableVar table(NewRequestId int);
+    INSERT INTO [dbo].[POI_PT_ChangeRequest_For_NPPlaces]
+      ([Operation],[Requestor],[FeatureJSON])
+        OUTPUT INSERTED.RequestId INTO @MyTableVar
+      VALUES (@Operation, @Requestor, @Feature);
+    SELECT NewRequestId FROM @MyTableVar;
+END
+GO
+
+
+-- View to combine Change Requests and ChangeRequest Actions to determine status of Change Request
+-- Grant SELECT to Public
+
+USE [akr_socio]
+GO
+
+/****** Object:  View [dbo].[POI_PT_ChangeRequestStatus_For_NPPlaces]    Script Date: 12/30/2014 3:57:45 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE VIEW [dbo].[POI_PT_ChangeRequestStatus_For_NPPlaces] AS 
+select CR.RequestId, A.[TimeStamp], [Action], Comment from POI_PT_ChangeRequest_For_NPPlaces as CR
+left join POI_PT_ChangeRequestAction_For_NPPlaces as A
+on CR.RequestId = A.RequestId
+
+
+GO
+
+
 
