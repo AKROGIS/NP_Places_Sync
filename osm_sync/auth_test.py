d675ce993e14f33c44e49e6b929ade77b1efd623
Regan Sarwas <regan_sarwas@nps.gov>
2015-06-08 15:20:10 -0800

    Fix the authorization to work with places-api

--- osm_sync/auth_test.py
+++ osm_sync/auth_test.py
@@ -2,38 +2,52 @@ import requests
 import urlparse
 
 from secrets import *
+import json
+
 
 def test():
-    request = False
-    authorize = False
+    request = True
+    authorize = True
     access = True
-    url = secrets['places']['url']
-    user = secrets['places']['user']
-    request_tokens = secrets['places']['request_tokens']
+    url = secrets['local']['url']
+    user = secrets['local']['user']
+    username = secrets['local']['username']
+    request_tokens = secrets['local']['request_tokens']
 
     if request:
         request_url = url + '/oauth/request_token'
         res = requests.post(request_url, None)
         print res
         print res.text
-        request_tokens = urlparse.parse_qs(res.text)
+        rough_tokens = urlparse.parse_qs(res.text)
+        # each item in the dict is a list with only one item
+        request_tokens = {}
+        for key in rough_tokens:
+            request_tokens[key] = rough_tokens[key][0]
         print request_tokens
 
     if authorize:
         auth_url = url + '/oauth/add_active_directory_user'
         auth_data = {
             'query': request_tokens,
-            'userId': user
+            'userId': user,
+            'name': username
         }
-        res = requests.post(auth_url, auth_data)
+        header = {
+            'Content-type': 'application/json',
+            'Accept': 'text/plain'
+        }
+        res = requests.post(auth_url, data=json.dumps(auth_data), headers=header)
         print res
         display_name = res.text
         print display_name
 
     if access:
         access_url = url + '/oauth/access_token'
-        print access_url
-        header = {'authorization': request_tokens}
+        auth = ('OAuth ' +
+                'oauth_token="' + request_tokens['oauth_token'] + '", ' +
+                'oauth_token_secret="' + request_tokens['oauth_token_secret'] + '"')
+        header = {'authorization': auth}
         res = requests.request('post', url=access_url, headers=header)
         print res
         print res.text
